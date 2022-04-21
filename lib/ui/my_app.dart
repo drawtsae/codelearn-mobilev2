1abd4be8071d5c2347a02dcb2f3f99f832a2627d
Hướng Nguyễn Đông <huong.nguyen@kyanon.digital>
2022-04-22 04:03:27 +0700

    upgrade dependency version

--- lib/ui/my_app.dart
+++ lib/ui/my_app.dart
@@ -62,7 +62,7 @@ class MyApp extends StatelessWidget {
               // Built-in localization of basic text for Cupertino widgets
               GlobalCupertinoLocalizations.delegate,
             ],
-            home: SplashScreen(),
+            home: PostDetailScreen(),
           );
         },
       ),
