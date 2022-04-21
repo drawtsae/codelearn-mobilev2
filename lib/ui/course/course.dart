1f8a107d74e9af999d1c07808beaaf50e4cb00eb
Nhan Nguyen <nhannt.itech@gmail.com>
2022-04-22 00:16:24 +0700

    create: course page

--- lib/ui/course/course.dart
+++ lib/ui/course/course.dart
@@ -1,10 +1,41 @@
+import 'package:boilerplate/ui/course/widgets/course_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
 class CourseView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.pink,
+    return Stack(
+      children: <Widget>[
+        _handleErrorMessage(),
+        _buildMainContent(context),
+      ],
     );
   }
+}
+
+Widget _handleErrorMessage() {
+  return Observer(
+    builder: (context) {
+      //if (_postStore.errorStore.errorMessage.isNotEmpty) {
+      //  return _showErrorMessage(_postStore.errorStore.errorMessage);
+      //}
+
+      return SizedBox.shrink();
+    },
+  );
+}
+
+Widget _buildMainContent(BuildContext context) {
+  //return Observer(
+  //  builder: (context) {
+  //   return true == false
+  //       ? CustomProgressIndicatorWidget()
+  //       : Material(child: _buildHomeBody());
+  // },
+  //);
+  return Container(
+      child: ListView(
+    children: [CourseItem(content: "test")],
+  ));
 }
