785628d2543c767effd545aebc6b09360b833b42
Nhan Nguyen <nhannt.itech@gmail.com>
2022-05-14 14:35:45 +0700

    update: refactor post page

---
+++ lib/constants/url.dart
@@ -0,0 +1,1 @@
+const DEFAULT_POST_IMAGE_URL = 'https://i.ibb.co/4Vsxhz0/2.png';
--- lib/ui/post/post.dart
+++ lib/ui/post/post.dart
@@ -2,10 +2,17 @@ import 'package:boilerplate/data/network/apis/post_api.dart';
 import 'package:boilerplate/data/post_repository.dart';
 import 'package:boilerplate/di/components/service_locator.dart';
 import 'package:boilerplate/models/post/post.dart';
+import 'package:boilerplate/ui/post/post.constants.dart';
 import 'package:boilerplate/ui/post/widget/post_item.dart';
-import 'package:boilerplate/widgets/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:fluttericon/font_awesome5_icons.dart';
+import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
+
+import '../../constants/common.dart';
+import '../../constants/enums.dart';
+import '../../models/post/post_list.dart';
+import '../../widgets/shimmer_loading.dart';
 
 class PostView extends StatefulWidget {
   _PostViewState createState() => _PostViewState();
@@ -13,147 +20,120 @@ class PostView extends StatefulWidget {
 
 class _PostViewState extends State<PostView> {
   late PostRepository _postRepository;
-  // The controller for the ListView
-  late ScrollController _controller;
-  // At the beginning, we fetch the first 20 posts
-  int _pageNum = 1;
-  int _limitSize = 20;
-  String _keyword = '';
-  // There is next page or not
+  TextEditingController _searchField = TextEditingController();
+
+  // Defines States
   bool _hasNextPage = true;
+  int _pageNumber = 0;
+  List<Post> _posts = EMPTY_POST_LIST;
+  LoadingType _isLoading = LoadingType.None;
+  String _keyword = EMPTY_STRING;
 
-  // Used to display loading indicators when _firstLoad function is running
-  bool _isFirstLoadRunning = false;
-
-  // Used to display loading indicators when _loadMore function is running
-  bool _isLoadMoreRunning = false;
-
-  // This holds the posts fetched from the server
-  List<Post> _posts = [];
-
-  // This function will be called when the app launches (see the initState function)
-  void _firstLoad() async {
+  // Defines Actions
+  Future<void> _getPosts(
+      {String keyword = EMPTY_STRING,
+      List<String> categoryIds = EMPTY_LIST_STRING,
+      List<String> tagIds = EMPTY_LIST_STRING,
+      int pageSize = DEFAULT_PAGE_SIZE,
+      int pageNumber = DEFAULT_PAGE_NUMBER,
+      LoadingType loadingType = LoadingType.First}) async {
     setState(() {
-      _isFirstLoadRunning = true;
+      _isLoading = loadingType;
+      if (loadingType == LoadingType.First) _searchField.text = EMPTY_STRING;
     });
-    try {
-      final postList = await _postRepository.getPosts(
-          _keyword, [], [], 'News', true, true, _pageNum, _limitSize);
-      setState(() {
-        _posts = postList!.data!;
-        //_hasNextPage = postList.hasNextPage!;
-      });
-    } catch (err) {
-      print('Something went wrong');
-    }
+
+    PostList? response;
+    await Future.delayed(
+        // Whenever loading more, must have to wait 1 second to send the request
+        Duration(milliseconds: loadingType == LoadingType.First ? 0 : 1000),
+        () async {
+      response = await _postRepository.getPosts(_keyword, categoryIds, tagIds,
+          'Sharing', true, true, pageNumber, pageSize);
+    });
 
     setState(() {
-      _isFirstLoadRunning = false;
+      _posts =
+          (keyword != _keyword ? EMPTY_POST_LIST : _posts) + response!.data!;
+      _hasNextPage = response!.hasNextPage!;
+      _pageNumber = pageNumber;
+      _isLoading = LoadingType.None;
     });
   }
 
-  // This function will be triggered whenver the user scroll
-  // to near the bottom of the list view
-  void _loadMore() async {
-    if (_hasNextPage == true &&
-        _isFirstLoadRunning == false &&
-        _isLoadMoreRunning == false &&
-        _controller.position.extentAfter < 300) {
-      setState(() {
-        _isLoadMoreRunning = true; // Display a progress indicator at the bottom
-      });
-      _pageNum += 1; // Increase _page by 1
-      try {
-        final res = await _postRepository.getPosts(
-            _keyword, [], [], 'News', true, true, _pageNum, _limitSize);
+  // Defines handlers
+  void handleFirstLoad() async {
+    await _getPosts();
+  }
 
-        setState(() {
-          _posts.addAll(res!.data!);
-          _hasNextPage = res.hasNextPage!;
-        });
-      } catch (err) {
-        print('Something went wrong!');
-      }
-
-      setState(() {
-        _isLoadMoreRunning = false;
-      });
+  void handleLoadMore() async {
+    if (_hasNextPage && _isLoading == LoadingType.None) {
+      await _getPosts(
+          keyword: _keyword,
+          pageNumber: _pageNumber++,
+          loadingType: LoadingType.More);
     }
   }
 
+  void handleSearch() async {
+    await _getPosts(keyword: _searchField.text, loadingType: LoadingType.First);
+  }
+
+  // Main
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        _handleErrorMessage(),
-        _buildMainContent(context),
-      ],
-    );
-  }
-
-  Widget _handleErrorMessage() {
-    return Observer(
-      builder: (context) {
-        //if (_postStore.errorStore.errorMessage.isNotEmpty) {
-        //  return _showErrorMessage(_postStore.errorStore.errorMessage);
-        //}
-
-        return SizedBox.shrink();
-      },
-    );
+    return Scaffold(
+        extendBodyBehindAppBar: true,
+        floatingActionButton:
+            SpeedDial(animatedIcon: AnimatedIcons.menu_close, children: [
+          SpeedDialChild(
+            child: Icon(FontAwesome5.plus),
+            label: 'Create Post',
+          )
+        ]),
+        body: Stack(children: <Widget>[
+          _buildMainContext(context),
+        ]));
   }
 
   @override
   void initState() {
     super.initState();
     _postRepository = PostRepository(getIt<PostApi>());
-    _firstLoad();
-    _controller = new ScrollController()..addListener(_loadMore);
+    handleFirstLoad();
   }
 
-  @override
-  void dispose() {
-    _controller.removeListener(_loadMore);
-    super.dispose();
-  }
-
-  Widget _buildMainContent(BuildContext context) {
+  Widget _buildMainContext(BuildContext context) {
     return Container(
-      child: _isFirstLoadRunning
-          ? CustomProgressIndicatorWidget()
-          : Column(
-              children: [
-                Expanded(
-                  child: ListView.separated(
-                    controller: _controller,
-                    padding: EdgeInsets.all(25),
-                    itemBuilder: (context, index) =>
-                        PostItem(post: _posts[index]),
-                    separatorBuilder: (BuildContext context, int index) =>
-                        SizedBox(height: 15),
-                    itemCount: _posts.length,
-                  ),
-                ),
-                // when the _loadMore function is running
-                if (_isLoadMoreRunning == true)
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10, bottom: 40),
-                    child: Center(
-                      child: CircularProgressIndicator(),
-                    ),
-                  ),
-
-                // When nothing else to load
-                //if (_hasNextPage == false)
-                //  Container(
-                //    padding: const EdgeInsets.only(top: 30, bottom: 40),
-                //    color: Colors.amber,
-                //    child: Center(
-                //      child: Text('You have fetched all of the content'),
-                //    ),
-                //  ),
-              ],
-            ),
-    );
+        child: Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(20.0),
+          child: TextField(
+            controller: _searchField,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Search posts...',
+                suffixIcon: IconButton(
+                  onPressed: () => handleSearch(),
+                  icon: Icon(Icons.search),
+                )),
+          ),
+        ),
+        Expanded(
+            child: (_isLoading == LoadingType.First)
+                ? ShimmerLoading()
+                : LazyLoadScrollView(
+                    // isLoading: _isLoading == LoadingType.More,
+                    onEndOfPage: () => handleLoadMore(),
+                    child: ListView.separated(
+                        itemBuilder: (context, index) =>
+                            PostItem(post: _posts[index]),
+                        separatorBuilder: (BuildContext context, int index) =>
+                            SizedBox(height: 15),
+                        itemCount: _posts.length),
+                  )),
+        if (_isLoading == LoadingType.More) ShimmerLoading()
+      ],
+    ));
   }
 }
