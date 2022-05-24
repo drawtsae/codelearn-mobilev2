ad8ae1540cc258b6452e2892cac52a4d20996d0c
Nhan Nguyen <nhannt.itech@gmail.com>
2022-05-24 23:53:19 +0700

    feat: create training screen

--- lib/data/training_repository.dart
+++ lib/data/training_repository.dart
@@ -1,4 +1,3 @@
-import '../constants/common.dart';
 import '../models/training/training_list.dart';
 import 'network/apis/training_api.dart';
 
@@ -23,5 +22,37 @@ class TrainingRepository {
       pageSize,
       pageNumber,
     );
+  }
+
+  Future<bool?> createTraining(
+      String title,
+      String slug,
+      String summary,
+      String level,
+      String content,
+      String videoUrl,
+      String tags,
+      // This is for test cases areas
+      String in1,
+      String out1,
+      String in2,
+      String out2,
+      String in3,
+      String out3) async {
+    return await _api.createTraining(
+        title,
+        slug,
+        summary,
+        level,
+        content,
+        videoUrl,
+        tags,
+        // This is for test cases areas
+        in1,
+        out1,
+        in2,
+        out2,
+        in3,
+        out3);
   }
 }
